1041215645dfdf7ecaf82c51125e63a40923f50c
Gaythri47499 <gayathrisanjana075@gmail.com>
2026-09-06 16:34:16 +0530

    Allow Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -54,6 +54,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://meenamitra.vercel.app",
     ],
 
     allow_credentials=True,
